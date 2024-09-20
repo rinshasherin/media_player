e500dfd0134bd108c89b51c66f68fc3494632e19
rinshasherin <rinshasherinck2002@gmail.com>
2024-09-20 15:45:46 +0530

    'mediaplayer'

--- src/services/allApi.js
+++ src/services/allApi.js
@@ -1,6 +1,6 @@
 import axios, { Axios } from "axios";
 
-const base_url="http://localhost:3000"
+const base_url="https://mp-server-jg1m.onrender.com"
 
 export const addVideoApi=async(data)=>{
     return await axios.post(`${base_url}/videos`,data)
